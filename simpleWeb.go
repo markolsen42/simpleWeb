69e631ef581f06241c9b53cfa7cb7e416de19ffc
Mark Olsen <markol@datacom.co.nz>
2019-11-08 10:02:40 +1300

    editing files from the html

--- simpleWeb.go
+++ simpleWeb.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	//	"net/http"
 	"os"
 	"strings"
 )
@@ -43,20 +45,37 @@ func initialHandler(w http.ResponseWriter, r *http.Request) {
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
 	// should be starts with not contains TODO
+
 	changeEndpoint := "/change"
 	if strings.Contains(r.URL.Path, changeEndpoint) {
-		// get the posted body as a map
+
+		//this is possibly how to get data from application/x-www-form-urlencoded data - eg form post submit
+		r.ParseForm()
+		fmt.Println(r.Form)
+
+		// get the posted body as a map - this worked for a json post from postman
 		result := getBodyMap(r)
 		// print the html element of the body
+		fmt.Print(result)
 		fmt.Print(result["html"])
 		// get the part of the URL after /change this will be the name of the file written
 		afterChange := r.URL.Path[len(changeEndpoint)+1:]
 		// turn the html element of the body a interface{} into a string then a byte array
-		d1 := []byte(fmt.Sprintf("%v", result["html"]))
+		//d1 := []byte(fmt.Sprintf("%v", result["html"]))
+		d1 := []byte(r.Form["html"][0])
 		//write the file
 		ioutil.WriteFile("content/"+afterChange, d1, 0644)
 
-		jsonResp := "{\"Hello\":\"World\"}"
+		jsonResp := `<!DOCTYPE html>
+		<html>
+		   <head>
+			  <title>HTML Meta Tag</title>
+			  <meta http-equiv = "refresh" content = "2; url = /main.html" />
+		   </head>
+		   <body>
+			  <p>Working</p>
+		   </body>
+		</html>`
 		fmt.Fprintf(w, jsonResp)
 	}
 }
@@ -88,8 +107,12 @@ func loadInserts(splits []string, addScaffolding bool) string {
 
 func formatInsert(insertToken string, insertHtml string) string {
 	var pre = "<!-- from " + insertToken + "-->\n" + "<div style='border-style: dotted'>"
+	var editor = `<form action="/change/` + insertToken + `.html" method="post">
+	Html: <input type="text" name="html"><br>
+  <input type="submit" value="Submit">
+</form>`
 	var post = "</div>" + "<!-- end " + insertToken + "-->\n"
-	return pre + insertHtml + post
+	return pre + editor + insertHtml + post
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
